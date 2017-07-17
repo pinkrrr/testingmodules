b0b27a9d3edc8d11e11515241baa0ee3943c7989
roma grave <goraved@outlook.com>
2017-07-17 11:03:31 +0300

    Questions and answers displaying

--- TestingModule/Additional/Adding.cs
+++ TestingModule/Additional/Adding.cs
@@ -1,5 +1,4 @@
-﻿using System.Data.Entity;
-using System.Linq;
+﻿using System.Linq;
 using TestingModule.Models;
 
 namespace TestingModule.Additional
---
+++ TestingModule/Models/QueAns.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestingModule.Models
+{
+    public class QueAns
+    {
+        public int QuestionId { get; set; }
+        public int DisciplineId { get; set; }
+        public int LectureId { get; set; }
+        public int ModuleId { get; set; }
+        public string Question { get; set; }
+        public int AnswerId { get; set; }
+        public string Answer { get; set; }
+    }
+}
