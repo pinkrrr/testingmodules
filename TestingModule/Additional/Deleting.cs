b8c48d5b2f3d5674eb93354a2a3889bd9a3bc3cb
roma grave <goraved@outlook.com>
2017-07-29 15:04:00 +0300

    Deleting childs

--- TestingModule/Additional/Deleting.cs
+++ TestingModule/Additional/Deleting.cs
@@ -9,18 +9,51 @@ namespace TestingModule.Additional
         private readonly testingDbEntities _db = new testingDbEntities();
         public void DeleteDiscipline(int disciplineId)
         {
+            var lectures = _db.Lectures.Where(t => t.DisciplineId == disciplineId).Select(t => t.Id).ToList();
+            foreach (var lecture in lectures)
+            {
+                var modules = _db.Modules.Where(t => t.LectureId == lecture).Select(t => t.Id).ToList();
+                foreach (var module in modules)
+                {
+                    var questions = _db.Questions.Where(t => t.ModuleId == module).Select(t => t.Id).ToList();
+                    foreach (var que in questions)
+                    {
+                        _db.Answers.RemoveRange(_db.Answers.Where(t => t.QuestionId == que));
+                    }
+                    _db.Questions.RemoveRange(_db.Questions.Where(t => t.ModuleId == module));
+                }
+                _db.Modules.RemoveRange(_db.Modules.Where(t => t.LectureId == lecture));
+            }
+            _db.Lectures.RemoveRange(_db.Lectures.Where(t => t.DisciplineId == disciplineId));
             var disc = new Discipline() { Id = disciplineId };
             _db.Entry(disc).State = EntityState.Deleted;
             _db.SaveChanges();
         }
         public void DeleteLecture(int lectureId)
         {
+            var modules = _db.Modules.Where(t => t.LectureId == lectureId).Select(t => t.Id).ToList();
+            foreach (var module in modules)
+            {
+                var questions = _db.Questions.Where(t => t.ModuleId == module).Select(t => t.Id).ToList();
+                foreach (var que in questions)
+                {
+                    _db.Answers.RemoveRange(_db.Answers.Where(t => t.QuestionId == que));
+                }
+                _db.Questions.RemoveRange(_db.Questions.Where(t => t.ModuleId == module));
+            }
+
             var lec = new Lecture() { Id = lectureId };
             _db.Entry(lec).State = EntityState.Deleted;
             _db.SaveChanges();
         }
         public void DeleteModule(int moduleId)
         {
+            var questions = _db.Questions.Where(t => t.ModuleId == moduleId).Select(t => t.Id).ToList();
+            foreach (var que in questions)
+            {
+                _db.Answers.RemoveRange(_db.Answers.Where(t => t.QuestionId == que));
+            }
+            _db.Questions.RemoveRange(_db.Questions.Where(t => t.ModuleId == moduleId));
             var mod = new Module() { Id = moduleId };
             _db.Entry(mod).State = EntityState.Deleted;
             _db.SaveChanges();
@@ -40,12 +73,31 @@ namespace TestingModule.Additional
         }
         public void DeleteSpeciality(int specialityId)
         {
+            var groups = _db.Groups.Where(t => t.SpecialityId == specialityId).Select(t => t.Id).ToList();
+            foreach (var group in groups)
+            {
+                var students = _db.Students.Where(t => t.GroupId == group).Select(t => t.Id).ToList();
+                foreach (var student in students)
+                {
+                    var id = _db.Students.Where(t => t.Id == student).Select(t => t.AccountId).FirstOrDefault();
+                    _db.Accounts.RemoveRange(_db.Accounts.Where(t => t.Id == id));
+                }
+                _db.Students.RemoveRange(_db.Students.Where(t => t.GroupId == group));
+            }
+            _db.Groups.RemoveRange(_db.Groups.Where(t => t.SpecialityId == specialityId));
             var spc = new Speciality() { Id = specialityId };
             _db.Entry(spc).State = EntityState.Deleted;
             _db.SaveChanges();
         }
         public void DeleteGroup(int groupId)
         {
+            var students = _db.Students.Where(t => t.GroupId == groupId).Select(t => t.Id).ToList();
+            foreach (var student in students)
+            {
+                var id = _db.Students.Where(t => t.Id == student).Select(t => t.AccountId).FirstOrDefault();
+                _db.Accounts.RemoveRange(_db.Accounts.Where(t => t.Id == id));
+            }
+            _db.Students.RemoveRange(_db.Students.Where(t => t.GroupId == groupId));
             var grp = new Group() { Id = groupId };
             _db.Entry(grp).State = EntityState.Deleted;
             _db.SaveChanges();
