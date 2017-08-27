b33967c1762e914ee101e27dda4a08b073efe79c
roma grave <goraved@outlook.com>
2017-08-27 11:55:25 +0300

    Excel ordering fix; Redirectind letor to active statistics and back to admin panel; Redirecting student to active module and back when answered

---
+++ TestingModule/Additional/StudentPageHelper.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Web;
+using TestingModule.Controllers;
+using TestingModule.Models;
+using TestingModule.ViewModels;
+
+namespace TestingModule.Additional
+{
+    public class StudentPageHelper : StudentController
+    {
+        private readonly testingDbEntities _db = new testingDbEntities();
+        public List<DiscLecotorViewModel> StudentsDisciplinesList(ClaimsIdentity claimsIdentity)
+        {
+            IEnumerable<Lector> lectors = _db.Lectors.ToList();
+            List<DiscLecotorViewModel> viewModels = _db.Disciplines.Select(d => new DiscLecotorViewModel
+            {
+                Id = d.Id,
+                Name = d.Name
+            }
+            ).ToList();
+            foreach (var model in viewModels)
+            {
+                model.Lectors = lectors;
+                model.LectorId = _db.LectorDisciplines.Where(t => model.Id == t.DisciplineId).Select(t => t.LectorId)
+                    .FirstOrDefault();
+            }
+            var c = claimsIdentity.FindFirst(System.Security.Claims.ClaimTypes.NameIdentifier);
+            var login = c.Value.ToString();
+
+            var account = _db.Accounts.FirstOrDefault(t => t.Login == login).Id;
+            var student = _db.Students.FirstOrDefault(t => t.AccountId == account).Id;
+            var connect = _db.StudentDisciplines.Where(t => t.StudentId == student).Select(t => t.DisciplineId).ToList();
+            viewModels = viewModels.Where(t => connect.Contains(t.Id)).ToList();
+            return (viewModels);
+        }
+
+        public int? CheckActiveQuiz(ClaimsIdentity claimsIdentity)
+        {
+            var c = claimsIdentity.FindFirst(System.Security.Claims.ClaimTypes.NameIdentifier);
+            var login = c.Value.ToString();
+            var account = _db.Accounts.FirstOrDefault(t => t.Login == login).Id;
+            var student = _db.Students.FirstOrDefault(t => t.AccountId == account).Id;
+            var connect = _db.StudentDisciplines.Where(t => t.StudentId == student).Select(t => t.DisciplineId).ToList();
+            var group = _db.Students.FirstOrDefault(t => t.Id == student).GroupId;
+            var active = _db.LecturesHistories.Where(t => connect.Contains(t.DisciplineId) && t.EndTime == null).Select(t => t.Id)
+                .ToList();
+            if (active.Any())
+            {
+                var groupsToDisc =
+                    _db.LectureHistoryGroups.Where(t => active.Contains(t.LectureHistoryId) && t.GroupId == group);
+                if (groupsToDisc.Any())
+                {
+                    var activeModule = _db.ModuleHistories.FirstOrDefault(t => active.Contains(t.LectureHistoryId));
+                    if (activeModule != null)
+                    {
+                        var moduleQuestions = _db.Questions.Where(t => t.ModuleId == activeModule.ModuleId).Select(t => t.Id)
+                            .ToList();
+                        var studentResponses =
+                            _db.Responses.Where(t => t.StudentId == student &&
+                                                     t.LectureId == activeModule.LectureHistoryId &&
+                                                     moduleQuestions.Contains(t.QuestionId)).ToList();
+                        if (studentResponses.Count != moduleQuestions.Count)
+                        {
+                            return activeModule.ModuleId;
+                        }
+
+                    }
+                }
+            }
+            return null;
+        }
+    }
+}
--- TestingModule/Controllers/StudentController.cs
+++ TestingModule/Controllers/StudentController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Security.Claims;
 using System.Web;
 using System.Web.Mvc;
+using TestingModule.Additional;
 using TestingModule.Models;
 using TestingModule.ViewModels;
 
@@ -14,46 +15,16 @@ namespace TestingModule.Controllers
         // GET: Student
         public ActionResult Index()
         {
-            var db = new testingDbEntities();
-            IEnumerable<Lector> lectors = db.Lectors.ToList();
-            List<DiscLecotorViewModel> viewModels = db.Disciplines.Select(d => new DiscLecotorViewModel
+            var studePageHelper = new StudentPageHelper();
+            //Check for active quiz
+            var active = studePageHelper.CheckActiveQuiz(User.Identity as System.Security.Claims.ClaimsIdentity);
+            if (active != null)
             {
-                Id = d.Id,
-                Name = d.Name
+                return RedirectToAction("/" + active, "Quiz");
             }
-            ).ToList();
-            foreach (var model in viewModels)
-            {
-                model.Lectors = lectors;
-                model.LectorId = db.LectorDisciplines.Where(t => model.Id == t.DisciplineId).Select(t => t.LectorId)
-                    .FirstOrDefault();
-            }
-            var claimsIdentity = User.Identity as System.Security.Claims.ClaimsIdentity;
-            var c = claimsIdentity.FindFirst(System.Security.Claims.ClaimTypes.NameIdentifier);
-            var login = c.Value.ToString();
-
-            var account = db.Accounts.FirstOrDefault(t => t.Login == login).Id;
-            var student = db.Students.FirstOrDefault(t => t.AccountId == account).Id;
-            var connect = db.StudentDisciplines.Where(t => t.StudentId == student).Select(t => t.DisciplineId).ToList();
-            var group = db.Students.FirstOrDefault(t => t.Id == student).GroupId;
-            var active = db.LecturesHistories.Where(t => connect.Contains(t.DisciplineId) && t.EndTime == null).Select(t => t.Id)
-                .ToList();
-            if (active.Any())
-            {
-                var groupsToDisc =
-                    db.LectureHistoryGroups.Where(t => active.Contains(t.LectureHistoryId) && t.GroupId == group);
-                if (groupsToDisc.Any())
-                {
-                    var activeModule = db.ModuleHistories.FirstOrDefault(t => active.Contains(t.LectureHistoryId))
-                        .ModuleId;
-                    if (activeModule != null)
-                    {
-                        return RedirectToAction("/"+activeModule, "Quiz");
-                    }
-                }
-            }
-           
-            viewModels = viewModels.Where(t => connect.Contains(t.Id)).ToList();
+            //If no active quiz, or already answered
+            var viewModels =
+                studePageHelper.StudentsDisciplinesList(User.Identity as System.Security.Claims.ClaimsIdentity);
             return View(viewModels);
         }
     }
