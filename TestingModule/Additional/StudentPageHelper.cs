ccba56bb306939d326de7614698544f7b809b393
Bogdan Ostashevsky <bostashevskyi@techexpert.ua>
2018-02-09 13:01:06 +0200

    Check for available cumulative quiz on student's home page.

--- TestingModule/Additional/StudentPageHelper.cs
+++ TestingModule/Additional/StudentPageHelper.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using System.Web;
+using System.Web.DynamicData;
+using Microsoft.Owin.Security.Provider;
 using TestingModule.Controllers;
 using TestingModule.Models;
 using TestingModule.ViewModels;
@@ -15,29 +17,26 @@ namespace TestingModule.Additional
     public class StudentPageHelper
     {
         private readonly testingDbEntities _db = new testingDbEntities();
-        public List<DiscLecotorViewModel> StudentsDisciplinesList(ClaimsIdentity claimsIdentity)
+        public async Task<IEnumerable<DiscLectorCumulativeCheckViewModel>> StudentsDisciplinesList()
         {
-            IEnumerable<Lector> lectors = _db.Lectors.ToList();
-            List<DiscLecotorViewModel> viewModels = _db.Disciplines.Select(d => new DiscLecotorViewModel
-            {
-                DiscId = d.Id,
-                DiscName = d.Name
-            }
-            ).ToList();
-            foreach (var model in viewModels)
-            {
-                model.Lectors = lectors;
-                model.LectorId = _db.LectorDisciplines.Where(t => model.DiscId == t.DisciplineId).Select(t => t.LectorId)
-                    .FirstOrDefault();
-            }
-            var c = claimsIdentity.FindFirst(System.Security.Claims.ClaimTypes.NameIdentifier);
-            var login = c.Value.ToString();
-
-            var account = _db.Accounts.FirstOrDefault(t => t.Login == login).Id;
-            var student = _db.Students.FirstOrDefault(t => t.AccountId == account).Id;
-            var connect = _db.StudentDisciplines.Where(t => t.StudentId == student).Select(t => t.DisciplineId).ToList();
-            viewModels = viewModels.Where(t => connect.Contains(t.DiscId)).ToList();
-            return (viewModels);
+            var studentId = new AccountCredentials().GetStudentId();
+            List<DiscLectorCumulativeCheckViewModel> model =
+                await (from d in _db.Disciplines
+                       join sd in _db.StudentDisciplines on d.Id equals sd.DisciplineId
+                       where sd.StudentId == studentId
+                       join cq in _db.CumulativeQuizPasseds on sd.DisciplineId equals cq.DisciplineId into cqsjoin
+                       from cqs in cqsjoin.DefaultIfEmpty()
+                       where cqs.StudentId == studentId || cqs == null
+                       join ld in _db.LectorDisciplines on d.Id equals ld.DisciplineId
+                       join l in _db.Lectors on ld.LectorId equals l.Id into lectjoin
+                       select new DiscLectorCumulativeCheckViewModel()
+                       {
+                           DiscId = d.Id,
+                           DiscName = d.Name,
+                           Lectors = lectjoin,
+                           CumulativeQuizId = cqs.IsPassed != true && cqs != null ? cqs.Id : (int?)null
+                       }).ToListAsync();
+            return model;
         }
 
         public async Task<bool> StudentCanPass(int moduleHistoryId, int studentId)
